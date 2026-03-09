2d8f3873b2b7c47a12b360ba3f7d6903b49d917d
Muhammad Irfan <irfanmrfn28@gmail.com>
2026-03-09 17:24:27 +0700

    Api: Wired Infrastructure DbContext

--- Marketplace.Api/Program.cs
+++ Marketplace.Api/Program.cs
@@ -1,6 +1,15 @@
+using Marketplace.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string not found.");
+
+builder.Services.AddDbContext<MarketplaceDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
