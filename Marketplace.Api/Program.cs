01c63b2ce6408c1502ce366335e1299f5c66ac17
Muhammad Irfan <irfanmrfn28@gmail.com>
2026-03-11 13:55:10 +0700

    Api: Updated CORS URL

--- Marketplace.Api/Program.cs
+++ Marketplace.Api/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("ReactFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:5173")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
